6a6a81781d5ebabbdcc49b825395c0cfd7a00845
Ayush5ah <sahayush89@gmail.com>
2025-01-01 12:13:36 +0530

    Updated API logic_final

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+   await  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   });
+
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err.message);
